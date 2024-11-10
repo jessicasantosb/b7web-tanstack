2c7c3f198e8560c4683f049aadfd82ef152d8949
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-10 19:24:40 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TanStack Query",
+  description:
+    "Este é uma aplicação desenvolvida no curso de ReactJS da B7Web para aprender TanStack Query",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

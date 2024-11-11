d433803ff23304db345ecda9c97ed36739e8b033
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-11 10:30:54 -0300

    feat: fetch a simple query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { PostProps } from "@/types/Posts";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export default function Home() {
+  const posts = useQuery({
+    queryKey: ["posts"],
+    queryFn: async (): Promise<PostProps[]> => {
+      const result = await axios.get("https://dummyjson.com/posts");
+      return result.data.posts;
+    },
+  });
+
   return (
-    <>
-      <h1>TanStack</h1>
-    </>
+    <section>
+      <h1 className="text-center py-4">TanStack</h1>
+
+      {posts.data && (
+        <ul>
+          {posts.data.map(({ id, title }) => {
+            return <li key={id}>{title}</li>;
+          })}
+        </ul>
+      )}
+    </section>
   );
 }
---
+++ src/types/Posts.ts
@@ -0,0 +1,9 @@
+export type PostProps = {
+    id: number;
+    title: string;
+    body: string;
+    tags: string[];
+    reactions: { likes: number; dislikes: number };
+    views: number;
+    userId: number;
+};

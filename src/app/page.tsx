a40c3a89b2f8819e9e8235050257fc609be7e64e
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-12 15:35:31 -0300

    feat: handle pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,49 +4,64 @@ import { usePost, usePosts, useQuotes } from "@/utils/queries";
 import { useState } from "react";
 
 export default function Home() {
-  const [canLoadPosts, setCanLoadPosts] = useState(false);  
+  const [canLoadPosts, setCanLoadPosts] = useState(false);
+  const [page, setPage] = useState(0);
+  const limit = 3;
 
   const posts = usePosts(canLoadPosts);
   const post = usePost(1);
   const quotes = useQuotes();
 
   return (
-    <section>
+    <section className="p-4">
       <h1 className="text-center py-4">TanStack</h1>
+      <div>
+        <button onClick={() => setCanLoadPosts(!canLoadPosts)}>
+          carregar posts
+        </button>
+        <p>Limite: {limit}</p>
+        <p>Page: {page}</p>
+        <button
+          className="mr-2 p-2 border"
+          onClick={() => setPage(page === 0 ? 0 : page - 1)}
+        >
+          página anterior
+        </button>
+        <button className="p-2 border" onClick={() => setPage(page + 1)}>
+          próxima página
+        </button>
 
-      <button onClick={() => setCanLoadPosts(!canLoadPosts)}>
-        carregar posts
-      </button>
+        <h3 className="text-center py-4">POSTS</h3>
+        {posts.isLoading && <p>Carregando...</p>}
+        <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
+        {post.data && (
+          <ul>
+            {posts.data?.map((post) => {
+              return <li key={post.id}>{post.title}</li>;
+            })}
+          </ul>
+        )}
+      </div>
 
-      <h3 className="text-center py-4">POSTS</h3>
-      {posts.isLoading && <p>Carregando...</p>}
-      <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
+      <div>
+        <h3 className="text-center py-4">POST</h3>
+        <p>{post.isLoading && "Carregando..."}</p>
+        <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
+        <p>{post?.data?.title}</p>
+      </div>
 
-      {post.data && (
-        <ul>
-          {posts.data?.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ul>
-      )}
-
-      <h3 className="text-center py-4">POST</h3>
-      <p>{post.isLoading && "Carregando..."}</p>
-      <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
-
-      <p>{post?.data?.title}</p>
-
-      <h3 className="text-center py-4">QUOTES</h3>
-      {quotes.isLoading && <p>Carregando...</p>}
-      <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
-
-      {quotes.data && (
-        <ul>
-          {quotes?.data?.map((quote) => {
-            return <li key={quote.id}>{quote.quote}</li>;
-          })}
-        </ul>
-      )}
+      <div>
+        <h3 className="text-center py-4">QUOTES</h3>
+        {quotes.isLoading && <p>Carregando...</p>}
+        <p>{!post.isLoading && post.isFetching && "Recarregando..."}</p>
+        {quotes.data && (
+          <ul>
+            {quotes?.data?.map((quote) => {
+              return <li key={quote.id}>{quote.quote}</li>;
+            })}
+          </ul>
+        )}
+      </div>
     </section>
   );
 }

aeaf1ec4430d14bb06f8daab9876f34ea70f9d46
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-12 15:16:11 -0300

    feat: handle query client

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -2,14 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getPost, getPosts, getQuotes } from "./api";
 
 export const usePosts = () =>
-  useQuery({ queryKey: ["posts"], queryFn: getPosts, staleTime: 2000 });
+  useQuery({ queryKey: ["posts"], queryFn: getPosts });
 
 export const usePost = (id: number) =>
   useQuery({
     queryKey: ["posts", id],
     queryFn: () => getPost(id),
-    staleTime: 5 * (60 * 1000), // 5 minutes
   });
 
 export const useQuotes = () =>
-  useQuery({ queryKey: ["quotes"], queryFn: getQuotes, staleTime: Infinity });
+  useQuery({ queryKey: ["quotes"], queryFn: getQuotes });

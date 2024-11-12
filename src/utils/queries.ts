4dc46ebfc4370e65a847bbcf78ef2f0400433123
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-12 15:10:37 -0300

    feat: add stale time

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -2,13 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getPost, getPosts, getQuotes } from "./api";
 
 export const usePosts = () =>
-  useQuery({ queryKey: ["posts"], queryFn: getPosts });
+  useQuery({ queryKey: ["posts"], queryFn: getPosts, staleTime: 2000 });
 
 export const usePost = (id: number) =>
   useQuery({
     queryKey: ["posts", id],
     queryFn: () => getPost(id),
+    staleTime: 5 * (60 * 1000), // 5 minutes
   });
 
 export const useQuotes = () =>
-  useQuery({ queryKey: ["quotes"], queryFn: getQuotes });
+  useQuery({ queryKey: ["quotes"], queryFn: getQuotes, staleTime: Infinity });

346a91dd593e151d83395b3ddd17cadf9f101759
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-13 10:42:47 -0300

    feat: render new data without a new fetching

--- src/utils/mutations.ts
+++ src/utils/mutations.ts
@@ -1,4 +1,20 @@
+// import { PostProps } from "@/types/Posts";
 import { useMutation } from "@tanstack/react-query";
 import { addPost } from "./api";
+import { queryClient } from "./queryClient";
 
-export  const useAddPost = () => useMutation({ mutationFn: addPost });
+export const useAddPost = () => {
+  return useMutation({
+    mutationFn: addPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+
+    // render new data without a new fetching
+
+    // onSuccess: (newPost: PostProps) => {
+    //   const posts = queryClient.getQueryData(["posts"]) as PostProps[];
+    //   queryClient.setQueryData(["posts"], [newPost, ...posts]);
+    // },
+  });
+};
